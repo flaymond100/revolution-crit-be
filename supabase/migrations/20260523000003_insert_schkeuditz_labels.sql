f1d272ad77d5575b6fd36c4bd1d797784c8b44c7
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2026-05-23 19:42:13 +0200

    sql

--- supabase/migrations/20260523000003_insert_schkeuditz_labels.sql
+++ supabase/migrations/20260523000003_insert_schkeuditz_labels.sql
@@ -1,9 +1,9 @@
 INSERT INTO public.schkeuditz_categories (id, label, detail, sort_order) VALUES
   ('lauf_5km',         '5km-Lauf',          '5 Runden',    1),
   ('lauf_10km',        '10km Lauf',          '10 Runden',   2),
-  ('u13m_u15w',        'U13m / U15w',        '20 Runden',   3),
-  ('u15m_u17w',        'U15m / U17w',        '25 Runden',   4),
-  ('u17m_masters4',    'U17m / Masters 4',   '35 Runden',   5),
+  ('u13m_u15w',        'U13m/U15w',          '20 Runden',   3),
+  ('u15m_u17w',        'U15m/U17w',          '25 Runden',   4),
+  ('u17m_masters4',    'U17m/Masters 4',     '35 Runden',   5),
   ('masters_2_3',      'Masters 2/3',        '45 Runden',   6),
   ('jedermann_leicht', 'Jedermann leicht',   '45 Minuten',  7),
   ('kids_races',       'Kids Races',         '',            8),
